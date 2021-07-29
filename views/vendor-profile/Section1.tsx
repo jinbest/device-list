0e1427fb077cd0057558e73e9ae24c7a81fb2426
jinbest <jin.bestvictoria718@gmail.com>
2021-07-30 05:00:49 +0800

    Redirect when there is not any matched route, remove breadcrumb in vendor page

--- views/vendor-profile/Section1.tsx
+++ views/vendor-profile/Section1.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
-import RightArrow from "../../components/svg/right-arrow"
-import _, { isEmpty, capitalize } from "lodash"
-import statesData from "../../const/statesData"
+// import RightArrow from "../../components/svg/right-arrow"
+import _, { capitalize } from "lodash"
+// import statesData from "../../const/statesData"
 import { vendorData } from "../../static/mock-data"
 import { useTranslation } from "react-i18next"
 import Rating from "@material-ui/lab/Rating"
@@ -16,8 +16,8 @@ import { getScore } from "../../service/hepler"
 const Section1 = () => {
   const [t] = useTranslation()
   const thisData = _.cloneDeep(vendorData)
-  const state = _.find(statesData[thisData.locInfo.country], { code: thisData.locInfo.state }),
-    stateName = !isEmpty(state) ? state.name : thisData.locInfo.state
+  // const state = _.find(statesData[thisData.locInfo.country], { code: thisData.locInfo.state }),
+  // stateName = !isEmpty(state) ? state.name : thisData.locInfo.state
   const score = getScore(thisData.data.reviews)
 
   const [favorite, setFavorite] = useState(false)
@@ -25,7 +25,7 @@ const Section1 = () => {
   return (
     <div className="vendor-profile-section1">
       <div className="container">
-        <div className="bread-crumbs">
+        {/* <div className="bread-crumbs">
           <p>Home</p>
           <RightArrow color="#4360FA" />
           <p>Find Location</p>
@@ -35,7 +35,7 @@ const Section1 = () => {
           <p>{capitalize(thisData.locInfo.city)}</p>
           <RightArrow color="#4360FA" />
           <p>{`${thisData.locInfo.name} - ${capitalize(thisData.locInfo.slug)}`}</p>
-        </div>
+        </div> */}
         <div className="vendor-profile-info">
           <div className="profile-info-logo">
             <img src={thisData.logo.img_src} alt={thisData.logo.alt} />
